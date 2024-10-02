8376e576d952e1b68d515d04dcf6513e74d069a5
Denis <Bhappy05@mail.ru>
2024-10-02 01:43:00 -0700

    Added Unit tests for launch screen and every view controller

--- RandomPhotoAppTests/RandomPhotoAppTests.swift
+++ RandomPhotoAppTests/RandomPhotoAppTests.swift
@@ -8,29 +8,220 @@
 import XCTest
 @testable import RandomPhotoApp
 
+class MockURLProtocol: URLProtocol {
+    static var testData: Data?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true // Intercept all requests
+    }
+
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request // We are not modifying the request
+    }
+
+    override func startLoading() {
+        if let data = MockURLProtocol.testData {
+            // Provide the mock data for the response
+            self.client?.urlProtocol(self, didLoad: data)
+        }
+        // Indicate that the loading has finished
+        self.client?.urlProtocolDidFinishLoading(self)
+    }
+
+    override func stopLoading() {
+        // No cleanup required for mock
+    }
+}
+    
 final class RandomPhotoAppTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        MockURLProtocol.testData = nil // Reset mock data
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDownWithError() throws { // Clean up resources and reset states
+        MockURLProtocol.testData = nil // Reset mock data
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testLaunchScreen() {
+        let launchScreen = UIStoryboard(name: "LaunchScreen", bundle: nil)
+        let viewController = launchScreen.instantiateInitialViewController()!
+        
+        let label = viewController.view.subviews.compactMap({ $0 as? UILabel }).first!
+        XCTAssertEqual(label.text, "Random Photo App \nby Denis Kovalev")
+        
+        let imageView = viewController.view.subviews.compactMap({ $0 as? UIImageView }).first!
+        XCTAssertNotNil(imageView.image)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test for image fetching
+    func testGetRandomPhoto_Success() async throws {	
+        let viewController = ViewController()
+        let urlString = "https://picsum.photos/300"
+        guard let url = URL(string: urlString) else {
+            XCTFail("Invalid URL")
+            return
+        }
+        
+        // Fetch image asynchronously
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        // Ensure valid response and data
+        XCTAssertNotNil(data, "Data should not be nil")
+        XCTAssertNotNil(response, "Response should not be nil")
+        
+        // Verify image creation from fetched data
+        let downloadedImage = UIImage(data: data)
+        XCTAssertNotNil(downloadedImage, "Downloaded image should not be nil")
+        
+        // Assign image to imageView in the view controller
+        DispatchQueue.main.async {
+            viewController.imageView.image = downloadedImage
+            XCTAssertNotNil(viewController.imageView.image, "ImageView should have an image")
         }
     }
-
+    
+    func makeMockSession() -> URLSession {
+        let config = URLSessionConfiguration.ephemeral
+        // Use MockURLProtocol for handling all network requests
+        config.protocolClasses = [MockURLProtocol.self]
+        return URLSession(configuration: config)
+    }
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test to simulate button tap and verify image changes
+    func testButtonTapChangesPhoto() async throws {
+        let viewController = ViewController()
+        
+        // Set the initial image
+        let initialImage = UIImage(systemName: "photo")!
+        viewController.imageView.image = initialImage
+        
+        // Set mock data for the new image
+        let newImage = UIImage(systemName: "star")!
+        MockURLProtocol.testData = newImage.pngData()
+        
+        // Use the custom session to simulate the image fetch
+        let session = makeMockSession()
+        viewController.didTapButtonPhoto()// Tap button with simulating image load
+        
+        // Wait for the image download to complete
+        try await Task.sleep(nanoseconds: 2_000_000_000)
+        
+        // Ensure the image has changed
+        XCTAssertNotEqual(viewController.imageView.image, initialImage, "Image should change after button tap")
+    }
+    
+    //
+    // Unit tests for CatsViewController
+    //
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test for image fetching
+    func testGetRandomCat_Success() async throws {
+        let viewController = CatsViewController()
+        let urlString = "https://cataas.com/cat?width=300&height=300"
+        guard let url = URL(string: urlString) else {
+            XCTFail("Invalid URL")
+            return
+        }
+        
+        // Fetch image asynchronously
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        // Ensure valid response and data
+        XCTAssertNotNil(data, "Data should not be nil")
+        XCTAssertNotNil(response, "Response should not be nil")
+        
+        // Verify image creation from fetched data
+        let downloadedImage = UIImage(data: data)
+        XCTAssertNotNil(downloadedImage, "Downloaded image should not be nil")
+        
+        // Assign image to imageView in the view controller
+        DispatchQueue.main.async {
+            viewController.imageView.image = downloadedImage
+            XCTAssertNotNil(viewController.imageView.image, "ImageView should have an image")
+        }
+    }
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test to simulate button tap and verify image changes
+    func testButtonTapChangesCat() async throws {
+        let viewController = CatsViewController()
+        
+        // Set the initial image
+        let initialImage = UIImage(systemName: "photo")!
+        viewController.imageView.image = initialImage
+        
+        // Set mock data for the new image
+        let newImage = UIImage(systemName: "star")!
+        MockURLProtocol.testData = newImage.pngData()
+        
+        // Use the custom session to simulate the image fetch
+        let session = makeMockSession()
+        viewController.didTapButtonCat()// Tap button with simulating image load
+        
+        // Wait for the image download to complete
+        try await Task.sleep(nanoseconds: 2_000_000_000)
+        
+        // Ensure the image has changed
+        XCTAssertNotEqual(viewController.imageView.image, initialImage, "Image should change after button tap")
+    }
+    
+    //
+    // Unit tests for DogsViewController
+    //
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test for image fetching
+    func testGetRandomDog_Success() async throws {
+        let viewController = DogsViewController()
+        let urlString = "https://placedog.net/300/300?id=5"
+        guard let url = URL(string: urlString) else {
+            XCTFail("Invalid URL")
+            return
+        }
+        
+        // Fetch image asynchronously
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        // Ensure valid response and data
+        XCTAssertNotNil(data, "Data should not be nil")
+        XCTAssertNotNil(response, "Response should not be nil")
+        
+        // Verify image creation from fetched data
+        let downloadedImage = UIImage(data: data)
+        XCTAssertNotNil(downloadedImage, "Downloaded image should not be nil")
+        
+        // Assign image to imageView in the view controller
+        DispatchQueue.main.async {
+            viewController.imageView.image = downloadedImage
+            XCTAssertNotNil(viewController.imageView.image, "ImageView should have an image")
+        }
+    }
+    
+    @MainActor // This allows us to remove the await keyword before the ImageViewController constructor since the unit test will now run in a Main actor-isolated context.
+    // Asynchronous test to simulate button tap and verify image changes
+    func testButtonTapChangesDog() async throws {
+        let viewController = DogsViewController()
+        
+        // Set the initial image
+        let initialImage = UIImage(systemName: "photo")!
+        viewController.imageView.image = initialImage
+        
+        // Set mock data for the new image
+        let newImage = UIImage(systemName: "star")!
+        MockURLProtocol.testData = newImage.pngData()
+        
+        // Use the custom session to simulate the image fetch
+        let session = makeMockSession()
+        viewController.didTapButtonDog()  // Tap button with simulating image load
+        
+        // Wait for the image download to complete
+        try await Task.sleep(nanoseconds: 2_000_000_000)
+        
+        // Ensure the image has changed
+        XCTAssertNotEqual(viewController.imageView.image, initialImage, "Image should change after button tap")
+    }
 }
